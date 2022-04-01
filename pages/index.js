cceeca51b4c5084bf108beaf7433fa7912a1fecc
pandemuliada <pandemuliada@gmail.com>
2022-04-01 23:40:06 +0800

    last read & prev & next chapter feature

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import Head from "next/head";
 import Link from "next/link";
 
 import TBATEChapters from "novels/the-beginning-after-the-end.json";
+import useLocalStorage from "use-local-storage";
 
 export default function Home() {
+  const [lastReadChapter, setLastReadChapter] = useLocalStorage(
+    "last_read_chapter/tbate"
+  );
+
+  console.log(lastReadChapter);
   return (
     <div>
       <Head>
@@ -21,15 +27,31 @@ export default function Home() {
       >
         <h1>The Beginning After The End</h1>
 
-        {TBATEChapters.map((chapter, index) => {
-          return (
-            <p>
-              <Link href={`/the-beginning-after-the-end/${index}`}>
-                <a>{chapter.title}</a>
-              </Link>
-            </p>
-          );
-        })}
+        {!!lastReadChapter && (
+          <Link href={`/the-beginning-after-the-end/${lastReadChapter}`}>
+            <a
+              style={{
+                display: "block",
+                background: "#dedede",
+                padding: 20,
+              }}
+            >
+              Last Read - {TBATEChapters[lastReadChapter].title}
+            </a>
+          </Link>
+        )}
+
+        <div>
+          {TBATEChapters.map((chapter, index) => {
+            return (
+              <p>
+                <Link href={`/the-beginning-after-the-end/${index}`}>
+                  <a>{chapter.title}</a>
+                </Link>
+              </p>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
--- pages/the-beginning-after-the-end/[id].js
+++ pages/the-beginning-after-the-end/[id].js
@@ -1,17 +1,28 @@
 import Head from "next/head";
 import TBATEChapters from "novels/the-beginning-after-the-end.json";
 import ReactMarkdown from "react-markdown";
+import { useEffect } from "react";
+import useLocalStorage from "use-local-storage";
+import Link from "next/link";
 
 const ChapterDetailPage = (props) => {
-  const { chapter, id } = props;
+  const { currentChapter, previousChapter, nextChapter, id } = props;
+
+  const [lastReadChapter, setLastReadChapter] = useLocalStorage(
+    "last_read_chapter/tbate"
+  );
+
+  useEffect(() => {
+    setLastReadChapter(id);
+  }, [id]);
 
   return (
     <>
       <Head>
-        <title>The Beginning After The End - {chapter.title}</title>
+        <title>The Beginning After The End - {currentChapter.title}</title>
         <meta
           name="description"
-          content={`The Beginning After The End - ${chapter.title}`}
+          content={`The Beginning After The End - ${currentChapter.title}`}
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -23,14 +34,56 @@ const ChapterDetailPage = (props) => {
           padding: "24px",
         }}
       >
-        <h1>The Beginning After The End - {chapter.title}</h1>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+        <h1>The Beginning After The End - {currentChapter.title}</h1>
 
         <div
           style={{
             lineHeight: "1.8rem",
           }}
         >
-          <ReactMarkdown children={chapter.content} />
+          <ReactMarkdown children={currentChapter.content} />
+        </div>
+
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: 24,
+          }}
+        >
+          {!!previousChapter ? (
+            <Link
+              href={`/the-beginning-after-the-end/${previousChapter.index}`}
+            >
+              <a
+                style={{
+                  display: "block",
+                  background: "#dedede",
+                  padding: 16,
+                }}
+              >
+                Prev : {previousChapter.title}
+              </a>
+            </Link>
+          ) : (
+            <div />
+          )}
+          {!!nextChapter && (
+            <Link href={`/the-beginning-after-the-end/${nextChapter.index}`}>
+              <a
+                style={{
+                  display: "block",
+                  background: "#dedede",
+                  padding: 16,
+                }}
+              >
+                Next : {nextChapter.title}
+              </a>
+            </Link>
+          )}
         </div>
       </div>
     </>
@@ -40,8 +93,31 @@ const ChapterDetailPage = (props) => {
 export default ChapterDetailPage;
 
 ChapterDetailPage.getInitialProps = async (ctx) => {
+  let previousChapter = null;
+  let nextChapter = null;
+
+  if (ctx.query.id > 0) {
+    previousChapter = {
+      index: parseInt(ctx.query.id) - 1,
+      ...TBATEChapters[parseInt(ctx.query.id) - 1],
+    };
+  } else {
+    previousChapter = null;
+  }
+
+  if (ctx.query.id == TBATEChapters.length - 1) {
+    nextChapter = null;
+  } else {
+    nextChapter = {
+      index: parseInt(ctx.query.id) + 1,
+      ...TBATEChapters[parseInt(ctx.query.id) + 1],
+    };
+  }
+
   return {
-    chapter: TBATEChapters[ctx.query.id],
     id: ctx.query.id,
+    currentChapter: TBATEChapters[ctx.query.id],
+    previousChapter,
+    nextChapter,
   };
 };
